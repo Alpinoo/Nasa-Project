d2bda2c13699ab3998a6dbbc845cf6fb14694311
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-07-12 22:43:11 +0300

    added csv file added promise to handle async reading csv

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,20 @@ const http = require('http')
 const PORT = process.env.PORT || 8000
 const app = require('./app')
 
-
 const server = http.createServer(app)
 
-server.listen(PORT, ()=>{
-    console.log(`Server started in port ${PORT}...`);
-})
+
+const {run} = require('./models/planetsModel')
+
+
+//!reading csv is async. So, we create an async function to read its data before server starts
+const startServer = async ()=>{
+    await run
+    server.listen(PORT, ()=>{
+        console.log(`Server started in port ${PORT}...`);
+    })
+}
+startServer()
+
 
 

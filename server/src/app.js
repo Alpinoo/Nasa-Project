cf2eec365d9eccf1bdd22b025dab3db9569177ca
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-07-13 03:08:48 +0300

    -changed build path to work both in same place -build file added with build command -cors changed because it didn't work -added configuration for back and front using same port -added deploy script to copy build and start server with it

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const planetsRouter = require('./routes/planets/planetsRouter')
 const cors = require('cors')
@@ -6,18 +7,14 @@ const cors = require('cors')
 const app = express()
 // app.use(cors())//if we do this, cors will allow all websites to access to the database.
 
-const whiteList = ['http://localhost:3000']
-app.use(cors({
-    origin: (origin,callback)=>{
-        if(whiteList.indexOf(origin) != -1){
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}))
+app.use(cors ({
+    origin: 'http://localhost:3000'
+    }))
 
 app.use(express.json())
+//?Now, port 8000 can be used either by front or backe (when running from server folder)
+app.use(express.static(path.join(__dirname,'..','public'))) //we use this for public folder and server uses the same port
+
 app.use(planetsRouter)
 
 module.exports = app

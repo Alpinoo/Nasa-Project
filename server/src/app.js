c134f5e4b8deaf70e24ac9f1871bb91497c3038d
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-07-13 03:29:30 +0300

    integrated morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const planetsRouter = require('./routes/planets/planetsRouter')
 const cors = require('cors')
+const morgan = require('morgan')
 
 
 const app = express()
@@ -9,10 +10,13 @@ const app = express()
 
 app.use(cors ({
     origin: 'http://localhost:3000'
-    }))
+}))
+
+//?morgan is for logging purposes. It logs what user done in the website
+app.use(morgan('combined'))
 
 app.use(express.json())
-//?Now, port 8000 can be used either by front or backe (when running from server folder)
+//?Now, port 8000 can be used either by front or back (when running from server folder)
 app.use(express.static(path.join(__dirname,'..','public'))) //we use this for public folder and server uses the same port
 
 app.use(planetsRouter)

f3c2560e3d584927695e78da70c3275735c7ed32
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-07-14 12:55:37 +0300

    error handling for launch added

--- server/src/routes/launches/launchesController.js
+++ server/src/routes/launches/launchesController.js
@@ -6,7 +6,17 @@ const getAllLaunches = (req,res)=>{
 
 const addLaunch = (req,res)=>{
     const launch = req.body
+    if(!launch.launchDate || !launch.mission || !launch.rocket || !launch.destination){
+        return res.status(400).json({
+            error: 'Some of the required fields are missing'
+        })
+    }
     launch.launchDate = new Date(launch.launchDate)
+    if(isNaN(launch.launchDate)){ //?Behind the scenes, js calls launchDate.values() and returns a number(timestamp) or NaN. If NaN, it means that it's not a date
+        return res.status(400).json({
+            error: 'Invalid date'
+        })
+    }
     const createdLaunch = createLaunch(launch)
     return res.status(201).json(createdLaunch)
 }
